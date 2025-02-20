70158c096db47ea644c9394659ce2810ef77d8da
Egor Terentev <eg.terentjev2016@gmail.com>
2025-02-20 18:02:20 +0700

    Добавил класс графического объекта

---
+++ LAB_1_JAVA/src/Circle.java
@@ -0,0 +1,23 @@
+public class Circle {
+    private int radius;
+    private String color;
+    public Circle() {
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
